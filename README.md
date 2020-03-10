96e531c962b40624ccd8e800b0403cfed7b25666
sINusBob <magno.alberto@live.com>
2020-03-10 13:58:26 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue003
-Created with CodeSandbox
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import "./app.scss";
+
+// Ensure you are using css-loader
+// import 'vuetify/dist/vuetify.min.css';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#root");
